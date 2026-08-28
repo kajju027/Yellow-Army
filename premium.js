681c5fa1207025f7d97ffde68c43bcede24e6c1d
ꕶᴀʏᴀɴ ᴏᴀɪ 🌐 <kajju3864@gmail.com>
2026-08-28 19:42:51 +0530

    Create premium.js

---
+++ premium.js
@@ -0,0 +1,3 @@
+window.__ad1 = 'https://pl31069087.profitableratecpmnetwork.com/bb/73/da/bb73dae2fb894c2f9cb3e6e3b12bda0c.js';
+window.__ad2 = 'https://pl31069088.profitableratecpmnetwork.com/ab/67/21/ab6721f720137f049e5f6cd32035eb90.js';
+window.__ad3 = 'https://www.profitableratecpmnetwork.com/ei8aztwyv?key=2e34a165c0b10bd1bb8fcf1a0615f2e0';
